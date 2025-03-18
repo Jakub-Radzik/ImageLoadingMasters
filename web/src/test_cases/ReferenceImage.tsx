320ee44419ba8e76686d160e2705493fd38fd957
Jakub Radzik <jradzik4@gmail.com>
2025-03-18 19:58:26 +0100

    deploy

--- web/src/test_cases/ReferenceImage.tsx
+++
@@ -1,8 +0,0 @@
-export const ReferenceImage = () => {
-  return (
-    <img
-      src="https://www.cdfa.ca.gov/v6.5/sample/images/gallery/orangecounty-big.jpg"
-      alt="alternative text"
-    />
-  );
-};
