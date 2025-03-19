1df325f8c89f1a6ca3e1f77bdd5725ef657c3d3e
Jakub Radzik <42437524+Jakub-Radzik@users.noreply.github.com>
2025-03-19 14:35:33 +0100

    convert scripts

---
+++ scale.js
@@ -0,0 +1,31 @@
+const { exec } = require('child_process');
+const path = require('path');
+
+const inputFile = process.argv[2];
+if (!inputFile) {
+  console.error('Usage: node index.js <input-file>');
+  process.exit(1);
+}
+
+const baseName = path.basename(inputFile, path.extname(inputFile));
+
+const sizes = ['1920', '800', '400'];
+
+
+for(const size of sizes) {
+      const resizeOption = size ? `-resize ${size}x` : '';
+      const resizeName = size ? `_w${size}` : '';
+
+      const outputFile = `${baseName}${resizeName}.png`;
+
+      const cmd = `convert "${inputFile}" ${resizeOption} "${outputFile}"`;
+    
+      exec(cmd, (err, stdout, stderr) => {
+        console.log(cmd)
+        if (err) {
+          console.error(`Error converting with compression`, err.message);
+        } else {
+          console.log(`Created: ${outputFile} (compression-leve)`);
+        }
+      });
+}
