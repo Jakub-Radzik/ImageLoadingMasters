e77e2292ce362c453b94aff80c5e79d9a246f0bc
Jakub Radzik <jradzik4@gmail.com>
2025-04-08 19:06:19 +0200

    fix

--- web/src/test_cases/Controls/ReferenceImageControls.tsx
+++ web/src/test_cases/Controls/ReferenceImageControls.tsx
@@ -23,42 +23,86 @@ export const ReferenceImageControls = () => {
   return (
     <div>
       <h2>Reference Image Controls</h2>
-        <div>
-            Image:
-            {
-                [1,2,3].map(v => <button style={img===v ? {background:'cyan'} : {}} onClick={()=>{updateSearchParam('img', v.toString())}}>{v}</button>)
-            }
-        </div>
-        <div>
-            Format:
-            {
-                ['png', 'avif', 'webp', 'jpg'].map(v => <button style={dir===v ? {background:'cyan'} : {}} onClick={()=>{updateSearchParam('dir', v)}}>{v}</button>)
-            }
-        </div>
-        <div>
-            Width:
-            {
-                [1920,800,400].map(v => <button style={w===v ? {background:'cyan'} : {}} onClick={()=>{updateSearchParam('w', v.toString())}}>{v}</button>)
-            }
-        </div>
-        <div>
-            Quality / Level:
-            {
-                [0,1,2,3,4,5,6,7,8,9,10,20,30,40,50,60,70,80,90].map(v => <button style={q == v ? {background:'cyan'} : {}} onClick={()=>{updateSearchParam('q', v.toString())}}>{v}</button>)
-            }
-        </div>
-        <div>
-            Compression:
-            {
-                ['lossy', 'lossless'].map(v => <button style={c == v ? {background:'cyan'} : {}} onClick={()=>{updateSearchParam('c', v.toString())}}>{v}</button>)
-            }
-        </div>
-        <div>
-            Interlacing:
-            {
-                [true, false].map(v => <button style={interlaced===v ? {background:'cyan'} : {}} onClick={()=>{updateSearchParam('interlaced', v.toString())}}>{v.toString()}</button>)
-            }
-        </div>
+      <div>
+        Image:
+        {[1, 2, 3].map((v) => (
+          <button
+            style={img === v ? { background: "cyan" } : {}}
+            onClick={() => {
+              updateSearchParam("img", v.toString());
+            }}
+          >
+            {v}
+          </button>
+        ))}
+      </div>
+      <div>
+        Format:
+        {["png", "avif", "webp", "jpg"].map((v) => (
+          <button
+            style={dir === v ? { background: "cyan" } : {}}
+            onClick={() => {
+              updateSearchParam("dir", v);
+            }}
+          >
+            {v}
+          </button>
+        ))}
+      </div>
+      <div>
+        Width:
+        {[1920, 800, 400].map((v) => (
+          <button
+            style={w === v ? { background: "cyan" } : {}}
+            onClick={() => {
+              updateSearchParam("w", v.toString());
+            }}
+          >
+            {v}
+          </button>
+        ))}
+      </div>
+      <div>
+        Quality / Level:
+        {[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 20, 30, 40, 50, 60, 70, 80, 90].map(
+          (v) => (
+            <button
+              style={q == v ? { background: "cyan" } : {}}
+              onClick={() => {
+                updateSearchParam("q", v.toString());
+              }}
+            >
+              {v}
+            </button>
+          )
+        )}
+      </div>
+      <div>
+        Compression:
+        {["lossy", "lossless"].map((v) => (
+          <button
+            style={c == v ? { background: "cyan" } : {}}
+            onClick={() => {
+              updateSearchParam("c", v.toString());
+            }}
+          >
+            {v}
+          </button>
+        ))}
+      </div>
+      <div>
+        Interlacing:
+        {[true, false].map((v) => (
+          <button
+            style={interlaced === v ? { background: "cyan" } : {}}
+            onClick={() => {
+              updateSearchParam("interlaced", v.toString());
+            }}
+          >
+            {v.toString()}
+          </button>
+        ))}
+      </div>
     </div>
   );
 };
