3e3590fc009dbc7e1c963d73eb1782fb785fc448
Jakub Radzik <jradzik4@gmail.com>
2025-04-18 15:28:39 +0200

    fix

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -2,16 +2,9 @@ import { Route, Routes } from "react-router-dom";
 import { TestPageLayout } from "./components/TestPageLayout";
 import { ReferenceImage } from "./test_cases/References/ReferenceImage";
 import { ReferenceImages } from "./test_cases/References/ReferenceImages";
-import { Survey } from "./survey/Survey";
-import {
-  fullImageComparisons,
-  fullSettings,
-  losslessImageComparisons,
-  pngSettings,
-} from "./survey/utils/settings";
-import { ResponsiveClownCar } from "./test_cases/Techniques/Responsive_ClownCar";
-import { directoryToUrlMap } from "./utils/url";
+
 import { TechniquesRoutes } from "./test_cases/TechniqueRoutes";
+import { PreloadImages } from "./test_cases/References/PreloadImages";
 
 function App() {
   const path = window.location.pathname;
@@ -23,6 +16,7 @@ function App() {
         <Routes>
           <Route path="/ref" element={<ReferenceImage />} />
           <Route path="/refs" element={<ReferenceImages />} />
+          <Route path="/pres" element={<PreloadImages />} />
 
           <Route path="/cdn2" element={<ReferenceImage httpVer={2} />} />
           <Route path="/cdn3" element={<ReferenceImage httpVer={3} />} />
